e629e81b903f72ac7a2cdb80a38cad3f1572dee4
Iqbal <zafar.iqbal@capgemini.com>
2018-01-18 17:47:35 +0000

    ContinousMVC

---
+++ ContinuousWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContinuousWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
